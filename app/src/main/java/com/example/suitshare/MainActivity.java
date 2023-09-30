1d263261412a7690fda875ab2582b21f5dcdd8de
suyogkad <7suyog@gmail.com>
2023-10-01 01:45:11 +0545

    Register/Main activity page created

--- app/src/main/java/com/example/suitshare/MainActivity.java
+++ app/src/main/java/com/example/suitshare/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.suitshare;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        // Button click listeners
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to start Login activity
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to start Register activity
+                Intent registerIntent = new Intent(MainActivity.this, register.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
