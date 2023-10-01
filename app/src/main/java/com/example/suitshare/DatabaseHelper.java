62baca78548d60614bbd539a6b75d131ebd26766
suyogkad <7suyog@gmail.com>
2023-10-02 02:36:20 +0545

    Add Iteam Activity Created

--- app/src/main/java/com/example/suitshare/DatabaseHelper.java
+++ app/src/main/java/com/example/suitshare/DatabaseHelper.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    // Users Table
     private static final String DATABASE_NAME = "users.db";
     private static final String TABLE_NAME = "user_table";
     private static final String COL_1 = "ID";
@@ -19,18 +20,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COL_7 = "PASSWORD";
     public static final String COL_8 = "AVATAR";
 
+    // Items Table
+    private static final String TABLE_ITEMS = "items";
+    private static final String COLUMN_ITEM_ID = "item_id";
+    private static final String COLUMN_USER_ID = "user_id";  // This will be linked to COL_1 of the user_table
+    private static final String COLUMN_ITEM_NAME = "item_name";
+    private static final String COLUMN_ITEM_DESCRIPTION = "item_description";
+    private static final String COLUMN_PURCHASE_STATUS = "purchase_status";
+    private static final String COLUMN_PRICE = "price";
+    private static final String COLUMN_DATE_ADDED = "date_added";
+    private static final String COLUMN_LOCATION_TAG = "location_tag";
+
+
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 3);
+        super(context, DATABASE_NAME, null, 4);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, FULLNAME TEXT, USERNAME TEXT UNIQUE, EMAIL TEXT UNIQUE, PHONE TEXT, DOB TEXT, PASSWORD TEXT, AVATAR TEXT)");
+
+        // Creating the Items table
+        db.execSQL("CREATE TABLE " + TABLE_ITEMS + " ("
+                + COLUMN_ITEM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_USER_ID + " INTEGER, "
+                + COLUMN_ITEM_NAME + " TEXT, "
+                + COLUMN_ITEM_DESCRIPTION + " TEXT, "
+                + COLUMN_PURCHASE_STATUS + " INTEGER, "
+                + COLUMN_PRICE + " REAL, "
+                + COLUMN_DATE_ADDED + " TEXT, "
+                + COLUMN_LOCATION_TAG + " TEXT, "
+                + "FOREIGN KEY (" + COLUMN_USER_ID + ") REFERENCES " + TABLE_NAME + "(" + COL_1 + "))");
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEMS);  // Dropping the Items table if it exists
         onCreate(db);
     }
 
